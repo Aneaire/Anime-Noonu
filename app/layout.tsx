80eacf65c88d08f78bc64cdc0a4b45074fb845a3
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-06-30 18:33:20 +0800

    just home no css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,35 @@
-import { GeistSans } from "geist/font/sans";
+import Sidebar from "@/components/common/Sidebar";
+import {
+  Jolly_Lodger,
+  Kaisei_Tokumin,
+  Montserrat,
+  Poppins,
+} from "next/font/google";
 import "./globals.css";
+
+const jollyLodger = Jolly_Lodger({
+  variable: "--font-jolly-lodger",
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+const kaisei = Kaisei_Tokumin({
+  variable: "--font-kaisei",
+  weight: ["400", "500", "800", "700"],
+  subsets: ["latin"],
+});
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -7,7 +37,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
+  title: "NOONU",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
@@ -17,10 +47,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html
+      lang="en"
+      className={`${poppins.variable} ${kaisei.variable} ${montserrat.variable} ${jollyLodger.variable} `}
+    >
       <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
+        <main className="min-h-screen flex min-w-screen overflow-x-hidden">
+          <Sidebar />
+          {/* <QueryProvider> */}
+          <section className=" w-full px-2 lg:px-5 pb-5">{children}</section>
+          {/* </QueryProvider> */}
         </main>
       </body>
     </html>
