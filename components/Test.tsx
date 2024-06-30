7414177a38e37955396fa078ca05de924d92bc8e
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-06-30 18:52:47 +0800

    add tanstack

--- components/Test.tsx
+++ components/Test.tsx
@@ -1,7 +1,10 @@
 import { useGetHomeAnime } from "@/utils/react-query/query";
 
 const Test = () => {
-  const { data } = useGetHomeAnime();
+  const { data, isLoading } = useGetHomeAnime();
+
+  if (isLoading) return <div>Loading...</div>;
+
   console.log(data);
   return <div>Test me baby</div>;
 };
