52bc986b84ecb006f2b2d308f379cac0d6880bec
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-07-01 14:08:54 +0800

    episodes added

--- app/anime/episodes/[id]/page.tsx
+++ app/anime/episodes/[id]/page.tsx
@@ -20,63 +20,66 @@ const Episodes = () => {
   if (infoLoading && episodesLoading) {
     return <div>Loading...</div>;
   }
-  console.log(episodes);
-  console.log(info);
+
   return (
-    <div className=" w-full  pt-5 ">
-      <div className="flex flex-col sm:flex-row gap-5 w-full section-container flex-wrap">
-        <div className="relative w-[250px] h-fit aspect-[11/16] mx-auto">
-          <Image
-            sizes="(100vw - 2rem) 100vw"
-            src={info!.anime.info.poster}
-            alt="cover"
-            fill
-            className=" rounded"
-          />
+    <div className=" w-full  pt-5 min-h-screen ">
+      {info && (
+        <div className="flex flex-col sm:flex-row gap-5 w-full section-container flex-wrap">
+          <div className="relative w-[250px] h-fit aspect-[11/16] mx-auto">
+            <Image
+              sizes="(100vw - 2rem) 100vw"
+              src={info!.anime.info.poster}
+              alt="cover"
+              fill
+              className=" rounded"
+            />
+          </div>
+          <div className=" flex-1">
+            <h5 className=" text-title-lg">{info?.anime.info.name}</h5>
+            <p
+              onClick={() => setViewDesc(!viewDesc)}
+              className={` description ${viewDesc ? "" : "line-clamp-6"}`}
+            >
+              {info?.anime.info.description}
+            </p>
+            <span className=" flex gap-x-2 mt-3 text-nowrap">
+              <GreenBadge>{info?.anime.info.stats.rating}</GreenBadge>
+              <RedBadge>S-{info?.seasons.length}</RedBadge>
+              <NormalBadge>{info?.anime.info.stats.episodes.sub}</NormalBadge>
+              <NormalBadge>{info?.anime.info.stats.quality}</NormalBadge>
+              <NormalBadge>{info?.anime.info.stats.duration}</NormalBadge>
+            </span>
+            <Divider />
+            <p className=" text-sm font-poppins pt-1 font-thin opacity-90">
+              Release Date : {info?.anime.moreInfo.aired}
+            </p>
+            <span className=" flex flex-wrap gap-y-1 gap-x-2 mt-2 text-nowrap">
+              <p className=" font-kaisei font-semibold text-sm">Genre : </p>
+              {info?.anime.moreInfo.genres.map((genre) => (
+                <NormalBadge>{genre}</NormalBadge>
+              ))}
+            </span>
+          </div>
         </div>
-        <div className=" flex-1">
-          <h5 className=" text-title-lg">{info?.anime.info.name}</h5>
-          <p
-            onClick={() => setViewDesc(!viewDesc)}
-            className={` description ${viewDesc ? "" : "line-clamp-6"}`}
-          >
-            {info?.anime.info.description}
-          </p>
-          <span className=" flex gap-x-2 mt-3 text-nowrap">
-            <GreenBadge>{info?.anime.info.stats.rating}</GreenBadge>
-            <RedBadge>S-{info?.seasons.length}</RedBadge>
-            <NormalBadge>{info?.anime.info.stats.episodes.sub}</NormalBadge>
-            <NormalBadge>{info?.anime.info.stats.quality}</NormalBadge>
-            <NormalBadge>{info?.anime.info.stats.duration}</NormalBadge>
-          </span>
-          <Divider />
-          <p className=" text-sm font-poppins pt-1 font-thin opacity-90">
-            Release Date : {info?.anime.moreInfo.aired}
-          </p>
-          <span className=" flex flex-wrap gap-y-1 gap-x-2 mt-2 text-nowrap">
-            <p className=" font-kaisei font-semibold text-sm">Genre : </p>
-            {info?.anime.moreInfo.genres.map((genre) => (
-              <NormalBadge>{genre}</NormalBadge>
-            ))}
-          </span>
-        </div>
-      </div>
+      )}
 
       {/* Episodes */}
-      <section className=" flex w-full">
-        <div className=" section-padding space-y-2 mt-5 basis-10/12">
-          {episodes?.episodes.map((episode) => (
-            <DisplayEpisodes
-              key={episode.episodeId}
-              number={episode.number}
-              title={episode.title}
-              episodeId={episode.episodeId}
-              isFiller={episode.isFiller}
-            />
-          ))}
-        </div>
-        <div></div>
-      </section>
+      {episodes && (
+        <section className=" flex w-full">
+          <div className=" section-padding space-y-2 mt-5 basis-10/12">
+            {episodes?.episodes.map((episode) => (
+              <DisplayEpisodes
+                key={episode.episodeId}
+                number={episode.number}
+                title={episode.title}
+                episodeId={episode.episodeId}
+                isFiller={episode.isFiller}
+              />
+            ))}
+          </div>
+          <div></div>
+        </section>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Test from "@/components/Test";
+import { Logo, LogoTitle } from "@/components/icons";
 import Home from "@/components/page/Home";
 
 export default async function Index() {
@@ -18,10 +19,10 @@ export default async function Index() {
 
   return (
     <div className="w-full relative section-padding">
-      {/* <span className=" scale-75 lg:scale-90 flex items-center gap-5 absolute top-1 left-0 lg:top-4 lg:left-5 z-10">
+      <span className=" scale-75 lg:scale-90 flex items-center gap-5 absolute top-1 left-0 lg:top-4 lg:left-5 z-10">
         <Logo />
         <LogoTitle />
-      </span> */}
+      </span>
       <Home />
       <Test />
     </div>
